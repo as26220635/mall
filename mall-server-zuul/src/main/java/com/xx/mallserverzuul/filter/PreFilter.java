2534eca72e50b4b0379d8e1afdd4606be338d029
ygx <851758629@qq.com>
2018-12-23 23:24:59 +0800

    更新

---
+++ mall-server-zuul/src/main/java/com/xx/mallserverzuul/filter/PreFilter.java
@@ -0,0 +1,34 @@
+package com.xx.mallserverzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 余庚鑫 on 2018/12/23
+ * PRE过滤器
+ */
+@Component
+public class PreFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
