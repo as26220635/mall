feba1e680ce66bc2cd412109e987446eb67fda8a
as26220635 <851758629@qq.com>
2018-12-31 17:14:25 +0800

    更新

--- mall-server-zuul/src/main/java/com/xx/mallserverzuul/filter/PreFilter.java
+++ mall-server-zuul/src/main/java/com/xx/mallserverzuul/filter/PreFilter.java
@@ -3,6 +3,7 @@ package com.xx.mallserverzuul.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.exception.ZuulException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
  * Created by 余庚鑫 on 2018/12/23
  * PRE过滤器
  */
+
+@RefreshScope
 @Component
 public class PreFilter extends ZuulFilter {
 
