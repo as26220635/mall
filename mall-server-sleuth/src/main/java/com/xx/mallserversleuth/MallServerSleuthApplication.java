ea993337d8b50597e1c81b570021f41150accc13
余庚鑫 <851758629@qq.com>
2019-01-08 23:17:29 +0800

    修改

--- mall-server-sleuth/src/main/java/com/xx/mallserversleuth/MallServerSleuthApplication.java
+++ mall-server-sleuth/src/main/java/com/xx/mallserversleuth/MallServerSleuthApplication.java
@@ -2,9 +2,11 @@ package com.xx.mallserversleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableZipkinServer
+@EnableEurekaClient
 @SpringBootApplication
 public class MallServerSleuthApplication {
 
